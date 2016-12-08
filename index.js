2fc1b8a8c1496cd137a7feb6cb528e3814faf635
obullxl <obullxl@163.com>
2016-12-08 19:25:30 +0800

    init

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const serveStatic = require('serve-static');
 const errorhandler = require('errorhandler');
 
 const app = express();
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('port', config['app_port'] || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -40,63 +40,14 @@ if('development' == app.get('env')) {
 	app.use(errorhandler());
 }
 
-/* 前台页面 */
-var front = require('./routes/front');
-app.get('/', front.index);
-app.get('/index.html', front.index);
-app.get('/index-:catg-:page.html', front.index);
-app.get('/index-:catg.html', front.index);
-
-app.get('/about.html', front.about);
-app.get('/album.html', front.album);
-
-app.get('/topic-:catg-:id.html', front.topic);
-app.post("/topic-reply", front.reply);
-
-/* 登录页面 */
-var index = require('./routes/index.js');
-app.get('/login.html', index.login);
-app.post('/login.html', index.login);
-app.get('/logout.html', index.logout);
-
-/* 用户页面 */
-var user = require('./routes/user');
-app.get('/admin/user-manage.html', user.manage);
-
-/* 文件配置 */
-var file = require('./routes/file');
-app.get('/file/select', file.select);
-app.post('/file/upload', file.upload);
-
-/* 后台管理 */
-var admin = require('./routes/admin');
-app.get('/admin', admin.index);
-app.get('/admin/index.html', admin.index);
-app.get('/admin/topic-create.html', admin.create);
-app.post('/admin/topic-store.html', admin.store);
-
-app.get('/admin/topic-view-:id.html', admin.view);
-app.get('/admin/topic-update-:id.html', admin.update);
-app.get('/admin/topic-manage.html', admin.manage);
-
-var album = require('./routes/album');
-app.get('/admin/album-create.html', album.create);
-app.post('/admin/album-create.html', album.create);
-
-app.get('/admin/album-update-:id.html', album.update);
-app.post('/admin/album-update-:id.html', album.update);
-
-app.post('/admin/image-create-:id.html', album.addImg);
-
-// 管理员中间件
-app.use("/admin", require("./lib/mware-admin.js")());
+/* ./routes/routes.js */
 
 // app.use(app.router);
 app.use(serveStatic(__dirname + '/public'));
 app.use(serveStatic(__dirname + '/upload'));
 
 // 创建服务端
-var server = app.listen(app.get('port'), function() {	
+app.listen(app.get('port'), function() {
 	console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++');
 	var env = process.env;
 	for(key in env) {
