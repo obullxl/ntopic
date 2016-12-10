0b27fcfc05f751fed1b674b9b6b4a3c83e61158c
obullxl <obullxl@163.com>
2016-12-10 13:06:03 +0800

    config

---
+++ config/config-dev.js
@@ -0,0 +1,27 @@
+#!/bin/env node
+
+/**
+ * 应用配置信息
+ */
+module.exports = {
+	/* 端口 */
+	'app_port': process.env.APP_PORT,
+
+	/* 是否使用HTTPS */
+	'use_ssl': false,
+	'web_protocal': 'http://',
+	
+	/* 静态资源 */
+	'static_host': 'http://obullxl.github.io',
+	
+	/* 数据库配置参数 */
+	'db_type': process.env.DB_TYPE || 'sqlite',
+	'db_file': process.env.DB_FILE || global.ROOT + '/ntopic.sqlite',
+
+	/* 日志配置 */
+	'log_type': 'console',
+	'log_level': 0,
+	
+	/* 广告推荐显示开关 */
+	'ads_show': process.env.ADS_SHOW || false
+};
